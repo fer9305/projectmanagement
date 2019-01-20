586afdf3b61d32245ca92f540cd19405c05fccab
Fernando <fercalderon37@gmail.com>
2019-01-19 20:20:56 -0700

    add crud operations for operations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,47 @@
 class ProjectsController < ApplicationController
-  def index; end
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      redirect_to edit_project_path(@project), error: 'There was an error updating project'
+    end
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      redirect_to new_project_path, error: 'There was an error'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: 'Project was successfully destroyed.'
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :start_date)
+  end
 end
